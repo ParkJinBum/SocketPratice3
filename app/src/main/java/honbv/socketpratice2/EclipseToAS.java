5682f5399f2d1c072898d9dff15c7e6de0604bda
ParkJinBum <honbv2@naver.com>
2017-10-20 20:44:23 +0900

    socket, Bottom Naviate, Navigate ok?

--- app/src/main/java/honbv/socketpratice2/EclipseToAS.java
+++ app/src/main/java/honbv/socketpratice2/EclipseToAS.java
@@ -11,6 +11,7 @@ import android.widget.Button;
 public class EclipseToAS extends Fragment {
 
 
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
--- app/src/main/java/honbv/socketpratice2/MainActivity.java
+++ app/src/main/java/honbv/socketpratice2/MainActivity.java
@@ -20,6 +20,11 @@ public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
